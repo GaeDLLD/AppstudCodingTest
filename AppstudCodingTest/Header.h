28c0269c1636278025ee8e6e3e5b5f1d9ab32f63
Gaëtan <serial.crossfire@gmail.com>
2017-11-02 14:14:16 +0100

    Add map view.

---
+++ AppstudCodingTest/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  AppstudCodingTest
+//
+//  Created by Gaëtan Delalande on 02/11/2017.
+//  Copyright © 2017 GaëTheDev. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <GoogleMaps/GoogleMaps.h>
+
+#endif /* Header_h */
